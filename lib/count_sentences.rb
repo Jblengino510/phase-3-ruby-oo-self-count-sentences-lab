ff77c5c6d56e4a1e6d9fe1d109916fdead188561
Joshua <j.blengino@yahoo.com>
2021-08-12 16:33:45 -0700

    commit tree 88f18766ef4d5db19db524711ba948ce73af7d10

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,29 @@ require 'pry'
 
 class String
 
+  def report_on_self
+    "Self is: #{self}"
+  end
+
+
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
-
+    split_str = self.split(/[.?!]\s/)
+    split_str.count
   end
+
 end
